5a76ec49b95b452a748722e0ed6afae96cf8333b
Bogdan Ponomarenko <ponomarenko.bogdan@yandex.ru>
2015-03-30 21:50:24 +0300

    complete

--- proj/js/main.js
+++ proj/js/main.js
@@ -7,6 +7,28 @@ var app = (function () {
         },
         setUpListeners: function () {
             $('.btn-up').on('click', app.scrollDown );
+            $('.showmenu').on('click', app.showMenu);
+            $('.section')
+                .waypoint(function (direction) {
+                    var activeItem = $(this.element).data('section');
+                    if (direction == 'down') {
+                        $('.nav__list-link').removeClass('active');
+                        $('#' + activeItem).addClass('active');
+                    }
+                }, {
+                    offset: '15%'
+                });
+            $('.section').waypoint(function (direction) {
+                    var activeItem = $(this.element).data('section');
+                    if (direction == 'up') {
+                        $('.nav__list-link').removeClass('active');
+                        $('#' + activeItem).addClass('active');
+                    }
+
+                }, {
+                    offset: '-60%'
+                });
+
         },
         scrollDown: function (e) {
             e.preventDefault();
@@ -18,6 +40,20 @@ var app = (function () {
             $('body').animate({
                 scrollTop: (destination - navHeight)
             }, 500);
+        },
+        showMenu: function (e) {
+            e.preventDefault();
+            var expression = parseInt($('.nav').css('left'));
+
+            if (!expression) {
+                $('.nav').animate({
+                    'left': '100%'
+                }, 300)
+            } else {
+                $('.nav').animate({
+                    'left': '0'
+                }, 300)
+            }
         }
     }
 })();
